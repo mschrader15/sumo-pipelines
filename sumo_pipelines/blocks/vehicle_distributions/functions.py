209bf0b78c9d5906bf883c26e024c42358d99df4
mschrader15 <mcschrader@crimson.ua.edu>
2023-02-07 21:09:23 -0600

    distribution function correction
    Signed-off-by: mschrader15 <mcschrader@crimson.ua.edu>

--- sumo_pipelines/blocks/vehicle_distributions/functions.py
+++ sumo_pipelines/blocks/vehicle_distributions/functions.py
@@ -23,14 +23,20 @@ def create_distribution_pandas(
         )
 
     if cf_config.table in DUMB_OBJECT_STORE:
-        samples = DUMB_OBJECT_STORE[cf_config.table]
+        samples = DUMB_OBJECT_STORE[cf_config.table].copy()
     else:
         samples = pd.read_csv(
             cf_config.table,
         )
         DUMB_OBJECT_STORE[cf_config.table] = samples
 
-    samples = samples.rename(columns=cf_config.cf_params)
+    samples = samples.rename(columns=cf_config.cf_params).sample(
+        cf_config.num_samples,
+        random_state=cf_config.seed
+    )
+    # add the vehicle type
+    vehicles = []
+    i = 0
 
     # add the vehicle type
     vehicles = []
@@ -40,8 +46,8 @@ def create_distribution_pandas(
         nonlocal i
         vehicles.append(
             f'\t<vType id="{cf_config.vehicle_distribution_name}_{i}" '
-            # + " ".join(['{}="{}"'.format(k, str(v)) for k, v in row.items()])
-            # + " "
+            + " ".join(['{}="{}"'.format(k, str(v)) for k, v in row.items()])
+            + " "
             + " ".join(['{}="{}"'.format(k, str(v)) for k, v in cf_config.additional_params.items()])
             + "/>"
         )
@@ -54,6 +60,59 @@ def create_distribution_pandas(
         for v in vehicles:
             f.write(v + "\n")
         f.write("</vTypeDistribution>")
+
+
+
+def create_independent_distribution_pandas(
+    cf_config: CFTableConfig,
+    config: DictConfig,
+) -> None:
+    if pandas_not_installed:
+        raise ImportError(
+            "pandas is not installed. Please install it to use this block."
+        )
+
+    if cf_config.table in DUMB_OBJECT_STORE:
+        samples = DUMB_OBJECT_STORE[cf_config.table].copy()
+    else:
+        samples = pd.read_csv(
+            cf_config.table,
+        )
+        DUMB_OBJECT_STORE[cf_config.table] = samples
+
+    samples = samples.rename(columns=cf_config.cf_params)
+    # add the vehicle type
+
+    sampled_params = {
+        col: samples[col].sample(
+           cf_config.num_samples,
+            random_state=cf_config.seed
+        ) for col in samples.columns
+    }
+
+    vehicles = [
+        (
+            (
+                f'\t<vType id="{cf_config.vehicle_distribution_name}_{i}" '
+                + " ".join(
+                    [f'{k}="{str(v[i])}"' for k, v in sampled_params.items()]
+                )
+            )
+            + " "
+        )
+        + " ".join(
+            [f'{k}="{str(v)}"' for k, v in cf_config.additional_params.items()]
+        )
+        + "/>"
+        for i in range(cf_config.num_samples)
+    ]
+
+    with open(cf_config.save_path, "w") as f:
+        f.write(f'<vTypeDistribution id="{cf_config.vehicle_distribution_name}" >\n')
+        for v in vehicles:
+            f.write(v + "\n")
+        f.write("</vTypeDistribution>")
+
 
 
 def create_simple_distribution(
