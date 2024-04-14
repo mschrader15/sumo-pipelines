3afdbd784e2ae645cabbe9d9b4b9b8af6c2caff9
Max <mcschrader@crimson.ua.edu>
2024-04-14 09:22:49 -0500

    added command line dotlist
    Signed-off-by: Max <mcschrader@crimson.ua.edu>

--- sumo_pipelines/execute.py
+++ sumo_pipelines/execute.py
@@ -13,13 +13,13 @@ from sumo_pipelines.run import run_pipeline
 @click.option("--replay", is_flag=True, default=False)
 @click.option("--skip-pipe-block", type=click.STRING, default="")
 def main(
-    config: List[str], debug: bool, gui: bool, replay: bool, skip_pipe_blocks: str
+    config: List[str], debug: bool, gui: bool, replay: bool, skip_pipe_block: str
 ) -> None:
     """Run the pipeline"""
     if isinstance(config, (tuple, list)):
         config = list(map(Path, config))
 
-    skip_pipe_blocks = skip_pipe_blocks.split(",")
+    skip_pipe_blocks = skip_pipe_block.split(",")
 
     run_pipeline(config, debug, gui, replay, skip_pipe_blocks)
 
